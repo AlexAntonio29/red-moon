aad3cc5950e71d5cd87ed2c07f6fbda715cec105
NECCHET <necchet@gmail.com>
2026-03-19 13:23:07 -0600

    regresando a hash 1d1dc5f y restauro

---
+++ assets/tiles_maps/nexus/IMG_alquimia.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Alchemy_Table_01-Sheet" tilewidth="32" tileheight="32" tilecount="132" columns="6">
+ <image source="sprites juego/Alchemy/Alchemy_Table_01-Sheet.png" width="192" height="704"/>
+</tileset>
