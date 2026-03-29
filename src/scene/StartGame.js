64a9bfdbfdb0bc1f2bd8aa39741a4e1daa2b98b7
AlexAntonio29 <alexantonioangel29@gmail.com>
2026-03-29 00:32:15 -0600

    add enemigos al mapa

--- src/scene/StartGame.js
+++ src/scene/StartGame.js
@@ -1500,7 +1500,7 @@ this.joystickCursors = this.joyStick.createCursorKeys();
 
     creacionEnemigosPosicionados(){
 
-      this.crearEnemigo(1,2950,8550,3);//cantidad Enemigos, x, y ,tipo de enemigo
+     // this.crearEnemigo(1,2950,8550,3);//cantidad Enemigos, x, y ,tipo de enemigo
 
       //this.crearEnemigo(1,3050,8600,1);
       //this.crearEnemigo(1,0,0,10);
@@ -1512,6 +1512,33 @@ this.joystickCursors = this.joyStick.createCursorKeys();
 
        //hacer prueba para jefe
 
+       
+      this.crearEnemigo(1,6605,7972,0);  
+      this.crearEnemigo(1,6290,8425,0);
+      this.crearEnemigo(1,5971,8451,3);
+      this.crearEnemigo(1,6515,8532,0);
+      this.crearEnemigo(1,6752,7657,0);
+      this.crearEnemigo(1,6684,7251,3);
+      this.crearEnemigo(1,6930,8392,0);
+      this.crearEnemigo(1,7518,8470,0);
+      this.crearEnemigo(1,7581,7905,3);
+      this.crearEnemigo(1,7857,8482,0);
+      this.crearEnemigo(1,8483,8509,0);
+      this.crearEnemigo(1,9063,8449,0);
+      this.crearEnemigo(1,9809,8107,3);
+      this.crearEnemigo(1,10109,8370,0);
+      this.crearEnemigo(1,10522,8296,0);
+      this.crearEnemigo(1,10134,8190,0);
+      this.crearEnemigo(1,9976,7865,0);
+      this.crearEnemigo(1,10561,7862,3);
+      this.crearEnemigo(1,10197,7506,0);
+      this.crearEnemigo(1,9596,7531,0);
+      this.crearEnemigo(1,8999,7645,0);
+      this.crearEnemigo(1,7658,7628,0);
+      this.crearEnemigo(1,8009,6957,0);
+
+      this.crearEnemigo(1,8187,8005,3);
+      this.crearEnemigo(1,8686,8033,3);
        
 
        
